637bd98f2221680cf5ad095ee027c9cf03720c37
Devin <devinatkin@gmail.com>
2023-12-28 18:33:20 -0700

    Initial Repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Coming Soon</h1>
+        <img src={logo} alt="logo" className="fade-out-image" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
