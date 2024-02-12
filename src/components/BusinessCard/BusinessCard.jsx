36430a3a58f82f1d04a3dcaaaf02049c0c2ff090
Devin <devinatkin@gmail.com>
2024-02-11 17:35:29 -0700

    Added QR Code on contact

---
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './BusinessCard.css';
+import data from './Business.json'; 
+
+import QRCode from "react-qr-code";
+class BusinessCard extends Component {
+    render() {
+        const vCardInfo = [
+            "BEGIN:VCARD",
+            "VERSION:3.0",
+            `N:${data.name}`,
+            `ADR:${data.address.replace(/,/g, ';')}`, // Simplistic way to handle address components; adjust as needed
+            `TEL:${data.phone}`,
+            // Assuming data.logoURL is a direct link to an image, otherwise, you might need to handle this differently
+            `PHOTO;TYPE=JPEG;VALUE=URI:${data.logoURL}`, 
+            "END:VCARD"
+        ].join("\r\n");
+        
+        return (
+            <div className="card">
+                <h2>{data.name}</h2>
+                <p><strong>Address:</strong> {data.address}</p>
+                <p><strong>Phone:</strong> {data.phone}</p>
+
+                <h3>Hours of Operation:</h3>
+                <p>{data.hours.weekdays}</p>
+                <p>{data.hours.weekends}</p>
+
+                <div style={{ height: "auto", margin: "0 auto", maxWidth: 128, width: "100%" }}>
+                    <QRCode
+                        size={128}
+                        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                        value={vCardInfo}
+                        viewBox={`0 0 128 128`}
+                    />
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default BusinessCard;
--- src/pages/Contact-Us.jsx
+++ src/pages/Contact-Us.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
+import BusinessCard from '../components/BusinessCard/BusinessCard';
 
 const ContactUs = () => {
     return (
         <div>
-            <h1> Contact Us Website Under Construction</h1>
+            <h1> Contact Us</h1>
             <p>If you have any questions or need to schedule an appointment, please feel free to contact us using the information below.</p>
 
-            <h2>Cochrane Veterinary Care Clinic</h2>
-            <p><strong>Address:</strong> 57 West Aarsby Road, Cochrane, Alberta, T4C 1M1</p>
-            <p><strong>Phone:</strong> (403) 932-4138</p>
-
-            <h3>Hours of Operation:</h3>
-            <p>Monday to Friday: 8:30 AM - 5:30 PM</p>
-            <p>Saturday & Sunday: Closed</p>
+            <BusinessCard />
 
             <p>We look forward to hearing from you and taking care of your beloved pets!</p>
 
